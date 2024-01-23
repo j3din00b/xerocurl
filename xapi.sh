8c3411ffe7e19109af14b1d09516856b21654355
DarkXero <81101439+DarkXero-dev@users.noreply.github.com>
2024-01-23 20:51:46 +0200

    Update xapi.sh

--- xapi.sh
+++ xapi.sh
@@ -5,4 +5,4 @@
 # Website 	: 	http://xerolinux.xyz
 ##################################################################################################################
 clear
-cd /tmp/ && git clone https://github.com/DarkXero-dev/scripts && cd scripts && chmod +x menu.sh && sh menu.sh
+cd /tmp/ && git clone https://github.com/DarkXero-dev/scripts && cd scripts && chmod +x menu.sh && sleep 3 && sh menu.sh
